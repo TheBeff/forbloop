afd4ee8475447bccf172b32a330695bccd64b0be
TheBeff <thebeff@gmail.com>
2017-02-14 17:16:00 -0500

    new title

---
+++ server/flick.js
@@ -0,0 +1,14 @@
+var $carousel = $('.carousel').flickity({
+  cellSelector: 'img',
+  imagesLoaded: true,
+  percentPosition: false,
+  pageDots: false
+});
+var $caption = $('.caption');
+// Flickity instance
+var flkty = $carousel.data('flickity');
+
+$carousel.on( 'select.flickity', function() {
+  // set image caption using img's alt
+  $caption.text( flkty.selectedElement.alt );
+});
